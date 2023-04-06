03697b8e8061c09192a828008e0808eb713367f0
DaniilShilov <shilovdanv@gmail.com>
2023-04-06 12:27:58 +0400

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+int Read(string number)
+{
+  int n = 0;
+  System.Console.WriteLine($"Введите {number}: ");
+  while(!int.TryParse(System.Console.ReadLine(), out n))
+  {
+    System.Console.WriteLine("Это не число!");
+  }
+  return n;
+}
+
+int Main()
+{
+  int number = Read("число");
+  int sum = 0;
+  while(number>0)
+  {
+    sum = sum + number % 10;
+    number = number / 10;
+  }
+  return sum;
+}
+void Answer()
+{
+  int x = Main();
+  System.Console.WriteLine($"Сумма всех цифр числа равняется: {x}");
+}
+
+Answer();
